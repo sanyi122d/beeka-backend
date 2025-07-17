348709973bcb969b81b2ef904c3171bb62ce0584
sanyi122d <sanyidiriba122@gmail.com>
2025-07-17 14:05:40 +0300

    first commit

--- firebase_auth.py
+++ firebase_auth.py
@@ -8,5 +8,6 @@ def verify_firebase_token(id_token):
     try:
         decoded_token = auth.verify_id_token(id_token)
         return decoded_token
+    
     except Exception as e:
-        return None
+        return None 
