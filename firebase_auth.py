c258df085094720e7f5b82604d7e9555bafc40c4
Sanyi Diriba <145164552+sanyi122d@users.noreply.github.com>
2025-07-21 20:03:29 +0300

    Update firebase_auth.py

--- firebase_auth.py
+++ firebase_auth.py
@@ -1,13 +1,16 @@
 import firebase_admin
 from firebase_admin import credentials, auth
+from firebase_admin import firestore
 
 cred = credentials.Certificate("backend/firebase_service_account.json")
 firebase_admin.initialize_app(cred)
+
+# Firestore client
+firestore_client = firestore.client()
 
 def verify_firebase_token(id_token):
     try:
         decoded_token = auth.verify_id_token(id_token)
         return decoded_token
-    
     except Exception as e:
-        return None 
+        return None
